4795ebdfcdc54cfec82c3b9538fc4f1b9b1fa221
Low Khye Ean <lowkhyeean@gmail.com>
2026-09-20 14:12:49 +1000

    Add: is-anagram - submission-4

---
+++ Data Structures & Algorithms/is-anagram/submission-4.py
@@ -0,0 +1,9 @@
+class Solution:
+    def isAnagram(self, s: str, t: str) -> bool:
+        freq_s = {}
+        for c in s:
+            freq_s[c] = freq_s.get(c, 0) + 1
+        freq_t = {}
+        for c in t:
+            freq_t[c] = freq_t.get(c, 0) + 1
+        return freq_s == freq_t
